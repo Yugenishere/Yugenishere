17b6cec6d96ac05a936d389f7feeda74e2862bd1
clarence-afk <140399801+Yugenishere@users.noreply.github.com>
2024-07-20 20:47:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -233,6 +233,13 @@ class App{
         
         return controllers;
     }
+    this.dolly = new THREE.Object3D();
+this.dolly.position.set(0, 0, 10);
+this.dolly.rotation.y = Math.PI; // Set initial rotation to 180 degrees around Y-axis
+this.dolly.add(this.camera);
+
+
+
     
     moveDolly(dt){
         if (this.proxy === undefined) return;
