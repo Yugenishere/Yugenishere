e1c7495745470379178fb910b313980a80e2d7ea
clarence-afk <140399801+Yugenishere@users.noreply.github.com>
2024-07-20 21:23:31 +0800

    loading Bar

--- libs/LoadingBar.js
+++ libs/LoadingBar.js
@@ -1,53 +1,62 @@
-class LoadingBar{
-	constructor(options){
-		this.domElement = document.createElement("div");
-		this.domElement.style.position = 'fixed';
-		this.domElement.style.top = '0';
-		this.domElement.style.left = '0';
-		this.domElement.style.width = '100%';
-		this.domElement.style.height = '100%';
-		this.domElement.style.background = '#000';
-		this.domElement.style.opacity = '0.7';
-		this.domElement.style.display = 'flex';
-		this.domElement.style.alignItems = 'center';
-		this.domElement.style.justifyContent = 'center';
-		this.domElement.style.zIndex = '1111';
-		const barBase = document.createElement("div");
-		barBase.style.background = '#aaa';
-		barBase.style.width = '50%';
-		barBase.style.minWidth = '250px';
-		barBase.style.borderRadius = '10px';
-		barBase.style.height = '15px';
-		this.domElement.appendChild(barBase);
-		const bar = document.createElement("div");
-		bar.style.background = '#22a';
-		bar.style.width = '50%';
-		bar.style.borderRadius = '10px';
-		bar.style.height = '100%';
-		bar.style.width = '0';
-		barBase.appendChild(bar);
-		this.progressBar = bar;
-		
-		document.body.appendChild(this.domElement);
-		
-		function onprogress(delta){
-			const progress = delta*100;
-			loader.progressBar.style.width = `${progress}%`;
-		}
-	}
-	
-	set progress(delta){
-		const percent = delta*100;
-		this.progressBar.style.width = `${percent}%`;
-	}
-	
-	set visible(value){
-		if (value){
-			this.domElement.style.display = 'flex';
-		}else{
-			this.domElement.style.display = 'none';
-		}
-	}
+
+	// Function to create an element with given tag name and optional classes
+function createElement(tagName, className) {
+    const element = document.createElement(tagName);
+    if (className) {
+        element.className = className;
+    }
+    return element;
+}
+
+class LoadingBar {
+    constructor(options) {
+        this.domElement = createElement('div');
+        Object.assign(this.domElement.style, {
+            position: 'fixed',
+            top: '0',
+            left: '0',
+            width: '100%',
+            height: '100%',
+            background: '#000',
+            opacity: '0.7',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            zIndex: '1111'
+        });
+
+        const barBase = createElement('div');
+        Object.assign(barBase.style, {
+            background: '#aaa',
+            width: '50%',
+            minWidth: '250px',
+            borderRadius: '10px',
+            height: '15px'
+        });
+
+        const bar = createElement('div');
+        Object.assign(bar.style, {
+            background: '#22a',
+            borderRadius: '10px',
+            height: '100%',
+            width: '0'
+        });
+
+        barBase.appendChild(bar);
+        this.domElement.appendChild(barBase);
+        this.progressBar = bar;
+
+        document.body.appendChild(this.domElement);
+    }
+
+    set progress(delta) {
+        const percent = delta * 100;
+        this.progressBar.style.width = `${percent}%`;
+    }
+
+    set visible(value) {
+        this.domElement.style.display = value ? 'flex' : 'none';
+    }
 }
 
 export { LoadingBar };
